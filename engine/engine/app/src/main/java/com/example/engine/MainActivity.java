888bcd1bc9bdbc3c9e0184a1b9495d64743149a1
davis <davis.emal@host.com>
2017-01-04 19:02:57 -0800

    add ffmpeg

--- engine/engine/app/src/main/java/com/example/engine/MainActivity.java
+++ engine/engine/app/src/main/java/com/example/engine/MainActivity.java
@@ -4,8 +4,18 @@ import android.app.NativeActivity;
 
 public class MainActivity extends NativeActivity {
 
+    final static String TAG = "MainActivity";
+
     static {
         System.loadLibrary("stlport_shared");
         System.loadLibrary("assimp");
+
+        //ffmpeg - sequence matters
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("swscale-4");
+        System.loadLibrary("avfilter-6");
     }
 }
