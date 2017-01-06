e6ea346ae28835970ba8d8dfe434c02cc89e2f49
davis <davis.emal@host.com>
2017-01-05 21:16:50 -0800

    add H264 decoder and sample code

--- engine/engine/app/src/main/cpp/app/main.cpp
+++ engine/engine/app/src/main/cpp/app/main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 #include "AppLog.h"
 #include "GLError.h"
+#include "H264_Decoder.h"
+#include "libavutil/pixfmt.h"
 
 
 extern "C" {
@@ -21,10 +23,13 @@ extern "C" {
 #include <libavutil/mathematics.h>
 #include <libavutil/samplefmt.h>
 
-
 }
 int init_display(struct engine *engine);
 void terminate_display(struct engine *engine);
+
+
+#define INBUF_SIZE 4096
+
 
 /**
  * Just the current frame in the display.
@@ -121,6 +126,40 @@ void initPlugins(struct engine *engine) {
 }
 #endif
 
+bool DecodeDone = false;
+int frameCount = 0;
+
+static void h264_callback(H264_DECODER_STATUS status, AVFrame* frame, AVPacket* pkt, void* user) {
+    if (status == DEC_STATUS_FINISH) {
+        DecodeDone = true;
+        return;
+    }
+    // LOGI("Got frame: %x, %x\n", frame, pkt);
+    // LOGI("Got frame: %d, %d\n", frame->width, frame->height);
+
+    uint8_t *out_buffer=(uint8_t *) av_malloc(avpicture_get_size(AV_PIX_FMT_YUV420P,
+                                                                 frame->width, frame->height));
+
+    int filled = avpicture_fill((AVPicture *) frame, out_buffer, AV_PIX_FMT_YUV420P, frame->width, frame->height);
+    // LOGI("filled = %d\n", filled);
+
+    LOGI("frame %d\n", frameCount++);
+
+    av_free(out_buffer);
+}
+
+static void decode_h264(const char *filePath) {
+    H264_Decoder decoder(h264_callback, NULL);
+    bool ret = decoder.load(filePath, 30.0f);
+    LOGI("load %s = %b\n", filePath, ret);
+    do {
+        ret = decoder.readFrame();
+        // LOGI("readFrame %s = %b\n", filePath, ret);
+        if (DecodeDone) {
+            break;
+        }
+    } while (true);
+}
 /*
  * Video encoding example
  */
@@ -266,6 +305,144 @@ static void video_encode_example(const char *filename, AVCodecID codec_id)
     printf("\n");
 }
 
+static void pgm_save(unsigned char *buf, int wrap, int xsize, int ysize,
+                     char *filename)
+{
+    FILE *f;
+    int i;
+
+    f = fopen(filename,"w");
+    fprintf(f, "P5\n%d %d\n%d\n", xsize, ysize, 255);
+    for (i = 0; i < ysize; i++)
+        fwrite(buf + i * wrap, 1, xsize, f);
+    fclose(f);
+}
+
+static int decode_write_frame(const char *outfilename, AVCodecContext *avctx,
+                              AVFrame *frame, int *frame_count, AVPacket *pkt, int last)
+{
+    int len, got_frame;
+    char buf[1024];
+
+    len = avcodec_decode_video2(avctx, frame, &got_frame, pkt);
+    if (len < 0) {
+        fprintf(stderr, "Error while decoding frame %d\n", *frame_count);
+        return len;
+    }
+    if (got_frame) {
+        printf("Saving %sframe %3d\n", last ? "last " : "", *frame_count);
+        fflush(stdout);
+
+        /* the picture is allocated by the decoder, no need to free it */
+        snprintf(buf, sizeof(buf), outfilename, *frame_count);
+        pgm_save(frame->data[0], frame->linesize[0],
+                 frame->width, frame->height, buf);
+        (*frame_count)++;
+    }
+    if (pkt->data) {
+        pkt->size -= len;
+        pkt->data += len;
+    }
+    return 0;
+}
+
+static void video_decode_example(const char *outfilename, const char *filename)
+{
+    AVCodec *codec;
+    AVCodecContext *c= NULL;
+    int frame_count;
+    FILE *f;
+    AVFrame *frame;
+    uint8_t inbuf[INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
+    AVPacket avpkt;
+
+    av_init_packet(&avpkt);
+
+    /* set end of buffer to 0 (this ensures that no overreading happens for damaged MPEG streams) */
+    memset(inbuf + INBUF_SIZE, 0, AV_INPUT_BUFFER_PADDING_SIZE);
+
+    printf("Decode video file %s to %s\n", filename, outfilename);
+
+    /* find the MPEG-1 video decoder */
+    codec = avcodec_find_decoder(AV_CODEC_ID_MPEG1VIDEO);
+    if (!codec) {
+        fprintf(stderr, "Codec not found\n");
+        exit(1);
+    }
+
+    c = avcodec_alloc_context3(codec);
+    if (!c) {
+        fprintf(stderr, "Could not allocate video codec context\n");
+        exit(1);
+    }
+
+    if (codec->capabilities & AV_CODEC_CAP_TRUNCATED)
+        c->flags |= AV_CODEC_FLAG_TRUNCATED; // we do not send complete frames
+
+    /* For some codecs, such as msmpeg4 and mpeg4, width and height
+       MUST be initialized there because this information is not
+       available in the bitstream. */
+
+    /* open it */
+    if (avcodec_open2(c, codec, NULL) < 0) {
+        fprintf(stderr, "Could not open codec\n");
+        exit(1);
+    }
+
+    f = fopen(filename, "rb");
+    if (!f) {
+        fprintf(stderr, "Could not open %s\n", filename);
+        exit(1);
+    }
+
+    frame = av_frame_alloc();
+    if (!frame) {
+        fprintf(stderr, "Could not allocate video frame\n");
+        exit(1);
+    }
+
+    frame_count = 0;
+    for (;;) {
+        avpkt.size = fread(inbuf, 1, INBUF_SIZE, f);
+        if (avpkt.size == 0)
+            break;
+
+        /* NOTE1: some codecs are stream based (mpegvideo, mpegaudio)
+           and this is the only method to use them because you cannot
+           know the compressed data size before analysing it.
+
+           BUT some other codecs (msmpeg4, mpeg4) are inherently frame
+           based, so you must call them with all the data for one
+           frame exactly. You must also initialize 'width' and
+           'height' before initializing them. */
+
+        /* NOTE2: some codecs allow the raw parameters (frame size,
+           sample rate) to be changed at any frame. We handle this, so
+           you should also take care of it */
+
+        /* here, we use a stream based decoder (mpeg1video), so we
+           feed decoder and see if it could decode a frame */
+        avpkt.data = inbuf;
+        while (avpkt.size > 0)
+            if (decode_write_frame(outfilename, c, frame, &frame_count, &avpkt, 0) < 0)
+                exit(1);
+    }
+
+    /* Some codecs, such as MPEG, transmit the I- and P-frame with a
+       latency of one frame. You must do the following to have a
+       chance to get the last frame of the video. */
+    avpkt.data = NULL;
+    avpkt.size = 0;
+    decode_write_frame(outfilename, c, frame, &frame_count, &avpkt, 1);
+
+    fclose(f);
+
+    avcodec_close(c);
+    av_free(c);
+    av_frame_free(&frame);
+    printf("\n");
+}
+
 /**
  * Main entry point, handles events
  */
@@ -285,8 +462,10 @@ void android_main(struct android_app *state) {
     extract_assets(state);
 
     avcodec_register_all();
-    video_encode_example("test.mpg", AV_CODEC_ID_MPEG1VIDEO);
+    // video_encode_example("test.mpg", AV_CODEC_ID_MPEG1VIDEO);
     // video_decode_example("test%02d.pgm", "test.mpg");
+    // video_decode_example("test%02d.pgm", "0000-0100.mp4");
+    decode_h264("0000-0100.avi");
 
     // Read all pending events.
     while (1) {
